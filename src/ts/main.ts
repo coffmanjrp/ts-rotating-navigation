9598f9df8ac1d947631981abc0c60c7743f47a0b
coffmanjrp <coffmanjrp@gmail.com>
2021-08-10 13:46:43 -0700

    Fix types

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,6 @@
-const menuOpen = document.getElementById('open')!;
-const menuClose = document.getElementById('close')!;
-const container = document.querySelector('.container')!;
+const menuOpen = document.getElementById('open') as HTMLButtonElement;
+const menuClose = document.getElementById('close') as HTMLButtonElement;
+const container = document.querySelector('.container') as HTMLDivElement;
 
 menuOpen.addEventListener('click', () => container.classList.add('show-nav'));
 menuClose.addEventListener('click', () =>
